fb980ae4b3e32595f674c9d7ae3e3817d0225581
errror-404 <60365507+errror-404@users.noreply.github.com>
2021-11-11 12:38:25 -0800

    V0.1
    En esta version solo tiene una funcionalidad de prueba que simila el pase de lista mediante arduino y enciende un led con arduino

---
+++ RFID_PASE_LISTA/RFID_PASE_LISTA/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿
+namespace RFID_PASE_LISTA
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnConsultar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtFechaHora = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtTarjeta = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtEmpleado = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(448, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(878, 415);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // btnConsultar
+            // 
+            this.btnConsultar.Location = new System.Drawing.Point(66, 351);
+            this.btnConsultar.Name = "btnConsultar";
+            this.btnConsultar.Size = new System.Drawing.Size(75, 23);
+            this.btnConsultar.TabIndex = 1;
+            this.btnConsultar.Text = "Leer";
+            this.btnConsultar.UseVisualStyleBackColor = true;
+            this.btnConsultar.Click += new System.EventHandler(this.btnConsultar_Click);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(207, 351);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(75, 23);
+            this.btnEliminar.TabIndex = 2;
+            this.btnEliminar.Text = "Cancelar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(90, 60);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(244, 23);
+            this.txtNombre.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Nombre";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 236);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 15);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Fecha/hora";
+            // 
+            // txtFechaHora
+            // 
+            this.txtFechaHora.Location = new System.Drawing.Point(90, 233);
+            this.txtFechaHora.Name = "txtFechaHora";
+            this.txtFechaHora.Size = new System.Drawing.Size(244, 23);
+            this.txtFechaHora.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(31, 174);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 15);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "#Tarjeta";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // txtTarjeta
+            // 
+            this.txtTarjeta.Location = new System.Drawing.Point(88, 174);
+            this.txtTarjeta.Name = "txtTarjeta";
+            this.txtTarjeta.Size = new System.Drawing.Size(244, 23);
+            this.txtTarjeta.TabIndex = 7;
+            this.txtTarjeta.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(15, 133);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 15);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "#Empleado";
+            // 
+            // txtEmpleado
+            // 
+            this.txtEmpleado.Location = new System.Drawing.Point(88, 125);
+            this.txtEmpleado.Name = "txtEmpleado";
+            this.txtEmpleado.Size = new System.Drawing.Size(244, 23);
+            this.txtEmpleado.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1338, 440);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtEmpleado);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtTarjeta);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtFechaHora);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnConsultar);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnConsultar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtFechaHora;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtTarjeta;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtEmpleado;
+    }
+}
+
---
+++ RFID_PASE_LISTA/RFID_PASE_LISTA/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace RFID_PASE_LISTA
+{
+    public partial class Form1 : Form
+        
+    {
+        SerialPort puertoAr;
+        public Form1()
+        {
+            puertoAr = new SerialPort();
+            InitializeComponent();
+            puertoAr.PortName = "COM4";
+            puertoAr.BaudRate = 9600;
+            puertoAr.DtrEnable = true;
+
+            try
+            {
+                puertoAr.Open();
+            }
+            catch (Exception x)
+            {
+
+                MessageBox.Show("Se debe de conectar Arduino " + x);
+            }
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.CenterToParent();
+        }
+
+        private void btnConsultar_Click(object sender, EventArgs e)
+        {
+            //Genera los numeros aleatorios
+            Random random = new Random();
+            int aleatorio = random.Next(1, 4);
+
+            //Se crean los AQrreglos de pruebas
+            string[] Nombres = {"Itzel Corrales","Alan Rodriguez","Miguel Arturo","Eliam Vazquez" };
+            int[] noEmpleado = { 1, 2, 3, 4 };
+            double[] noTarjeta = { 100001, 100002, 100003, 100004 };
+
+            //Se asignan los valores a los componentes
+            txtNombre.Text = Nombres[aleatorio];
+            txtEmpleado.Text = Convert.ToString( noEmpleado[aleatorio]);
+            txtTarjeta.Text = Convert.ToString(noTarjeta[aleatorio]);
+            txtFechaHora.Text = DateTime.Now.ToString();
+
+            puertoAr.Write("a");
+
+
+
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+
+            txtNombre.Text = "";
+            txtEmpleado.Text = "";
+            txtTarjeta.Text = "";
+            txtFechaHora.Text = "";
+
+            puertoAr.Write("b");
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
